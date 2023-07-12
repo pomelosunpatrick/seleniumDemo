c6ff042f617de9b0cc16f8af3c52ed4fa74bbea0
psu <psu@celum.com>
2023-07-12 09:13:47 +0200

    First commit for Selenium Cucumber Demo project.

---
+++ src/test/java/seleniumWebDriver/StepDefinitions.java
@@ -0,0 +1,55 @@
+package seleniumWebDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class StepDefinitions {
+
+    private final WebDriver driver = new FirefoxDriver();
+
+    @Given("I am on the DuckDuckGo search page")
+    public void I_visit_DuckDuckGo() {
+        driver.get("https://duckduckgo.com/");
+    }
+
+    @When("I search for {string}")
+    public void search_for(String query) {
+        WebElement element = driver.findElement(By.name("q"));
+        // Enter something to search for
+        element.sendKeys(query);
+        // Now submit the form. WebDriver will find the form for us from the element
+        element.submit();
+    }
+
+    @Then("the page title should start with {string}")
+    public void checkTitle(String titleStartsWith) {
+        // Google's search is rendered dynamically with JavaScript
+        // Wait for the page to load timeout after ten seconds
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until((ExpectedCondition<Boolean>) d -> {
+            assert d != null;
+            return d.getTitle().toLowerCase().startsWith(titleStartsWith);
+        });
+    }
+
+    @After()
+    public void closeBrowser(Scenario scenario) {
+        if (scenario.isFailed()) {
+            byte[] data =((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(data, "image/png", scenario.getName());
+        }
+        driver.quit();
+    }
+}
---
+++ src/test/resources/seleniumWebDriver/example.feature
@@ -0,0 +1,6 @@
+Feature: An example using Selenium WebDriver
+
+  Scenario: Finding some cheese
+    Given I am on the DuckDuckGo search page
+    When I search for "Cheese!"
+    Then the page title should start with "Cake"
