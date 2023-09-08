f425c07d603ae68dc7629b6d935c993ff23286a0
psu <psu@celum.com>
2023-09-08 10:11:23 +0200

    Add multiple browser tests in the feature file.

--- src/test/java/seleniumWebDriver/StepDefinitions.java
+++ src/test/java/seleniumWebDriver/StepDefinitions.java
@@ -10,6 +10,8 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -18,7 +20,24 @@ import java.time.Duration;
 
 public class StepDefinitions {
 
-    private final WebDriver driver = new FirefoxDriver();
+    private WebDriver driver;
+
+    @Given("I open the {string} browser")
+    public void openBrowser(String browser) {
+        switch (browser) {
+            case "Chrome":
+                driver = new ChromeDriver();
+                break;
+            case "Firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "Edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new RuntimeException("Unsupported browser type: " + browser);
+        }
+    }
 
     @Given("I am on the DuckDuckGo search page")
     public void I_visit_DuckDuckGo() {
--- src/test/resources/seleniumWebDriver/example.feature
+++ src/test/resources/seleniumWebDriver/example.feature
@@ -1,6 +1,14 @@
 Feature: An example using Selenium WebDriver
 
-  Scenario: Finding some cheese
+  Scenario Outline: Finding some cheese
+    # NOTE: Chrome is only supported up to version 114, hence commented out here
+    Given I open the <browser> browser
     Given I am on the DuckDuckGo search page
     When I search for "Cheese!"
     Then the page title should start with "Cheese?"
+
+    Examples:
+      | browser   |
+#      | "Chrome"  |
+      | "Firefox" |
+      | "Edge"    |
